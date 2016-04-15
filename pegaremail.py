a0aaebd02b5455e2ace83ee5db6344e12a2e14a8
Fred <frederico.sampaio@usp.br>
2016-04-15 10:11:14 -0300

    Provinha_2

--- pegaremail.py
+++ pegaremail.py
@@ -6,7 +6,7 @@ def html():
 <body>\n
   <h2>EMAIL</h2>\n
   <form method="get" action="/bin/login">\n
-    Email: <input type="text" name="user" size="25" /><br />\n
+    Email: <input type="text" name="user" size="25" /><br /><br />\n
     Assunto: <input type="text" name="sub" size="50" /><br /><br />\n  
     Conteudo: <input type="text" name="pw" size="100" /><br /><br />\n  
     <input type="submit" value="SEND" />\n
@@ -15,24 +15,28 @@ def html():
 </html>'''
 	
 def pegaemail(ip, porta):
-	serversocket=s.socket(s.AF_INET, s.SOCK_STREAM)
-	serversocket.bind((ip, porta))
-	serversocket.listen(1)
-	while True:
-		c, addr=serversocket.accept()
-		c.send(html().encode())
-		mensagem=c.recv(2048).decode()
-		c.close()
-		if "login?user=" in mensagem:
-			email=mensagem.split("user=")[1].split("&sub=")[0]
-			assunto=mensagem.split("&sub=")[1].split("&pw=")[0]
-			conteudo=mensagem.split("&pw=")[1].split(" ")[0]
-			break
-	serversocket.close()
-	email=email.replace("%40", "@")
-	assunto=assunto.replace("+", " ")
-	conteudo=conteudo.replace("+", " ")
-	return (email, assunto, conteudo)
+	try:
+		serversocket=s.socket(s.AF_INET, s.SOCK_STREAM)
+		serversocket.bind((ip, porta))
+		serversocket.listen(1)
+		while True:
+			c, addr=serversocket.accept()
+			c.send(html().encode())
+			mensagem=c.recv(2048).decode()
+			c.close()
+			if "login?user=" in mensagem:
+				email=mensagem.split("user=")[1].split("&sub=")[0]
+				assunto=mensagem.split("&sub=")[1].split("&pw=")[0]
+				conteudo=mensagem.split("&pw=")[1].split(" ")[0]
+				break
+		serversocket.close()
+		email=email.replace("%40", "@")
+		assunto=assunto.replace("+", " ")
+		conteudo=conteudo.replace("+", " ")
+		return (email, assunto, conteudo)
+	except OSError:
+		print("Impossivel se conectar.")
+		return('freddysampaio9@gmail.com', "Erro", "Nao foi possivel se concectar ao usuario")
 	
 if __name__=="__main__":
 	(email, assunto, conteudo)=pegaemail("172.26.182.43", 80)

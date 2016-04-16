a5c139c8b4fb1262383bc6f37d44cf41827bfd60
FredSampaio <freddy@linklocal.com.br>
2016-04-16 00:30:56 -0300

    Added files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: python provinha_2_MIME.py
--- provinha_2_MIME.py
+++ provinha_2_MIME.py
@@ -1,44 +1,71 @@
+import os
+from flask import Flask
+from flask import request
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
+
+
+app = Flask(__name__)
+def html():
+	return '''<html>\n
+<head><title>EMAIL</title></head>\n
+<body>\n
+  <h2>EMAIL</h2>\n
+  <form method="get" action="/bin/login">\n
+    Email: <input type="text" name="user" size="25" /><br /><br />\n
+    Assunto: <input type="text" name="sub" size="50" /><br /><br />\n  
+    Conteudo: <input type="text" name="pw" size="100" /><br /><br />\n  
+    <input type="submit" value="SEND" />\n
+  </form>\n
+</body>\n
+</html>'''
+
+@app.route("/")
+def index():
+    return html()
+
 #importa modulos de email
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEImage import MIMEImage
-
-import pegaremail
 import socket as s
 
-temp=s.socket(s.AF_INET, s.SOCK_DGRAM)
-temp.connect(("8.8.8.8",80))
-ip=temp.getsockname()[0]
-temp.close()
+def pegaemail(mensagem):
+	email=mensagem.split("user=")[1].split("&sub=")[0]
+	assunto=mensagem.split("&sub=")[1].split("&pw=")[0]
+	conteudo=mensagem.split("&pw=")[1].split(" ")[0]
+	email=email.replace("%40", "@")
+	assunto=assunto.replace("+", " ")
+	conteudo=conteudo.replace("+", " ")
+	return (email, assunto, conteudo)
 
-print(ip)
+def mandaemail2((email, assunto, content)):
+	app.config.update(dict(
+	    DEBUG = True,
+	    MAIL_SERVER = 'smtp.gmail.com',
+	    MAIL_PORT = 587,
+	    MAIL_USE_TLS = True,
+	    MAIL_USE_SSL = False,
+	    MAIL_USERNAME = 'freddysampaio9@gmail.com',
+	    MAIL_PASSWORD = 'fantauva',
+	))
+	mail = Mail(app)
+	msg = Message(
+	          assunto,
+	       sender = 'freddysampaio9@gmail.com',
+	       recipients = [email])
+	msg.body = content
+	mail.send(msg)
 
-email, assunto, content=pegaremail.pegaemail(ip, 80)
+@app.route("/bin/<mensagem>")
+def resposta(mensagem):
+	mandaemail2(pegaemail(str(request.url)))
+	return "<h1>Sucesso!</h1>"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
-# dados
-body = content
-sub = assunto
-mail_from = 'freddysampaio9@gmail.com'
-mail_to = email
-pwd = 'fantauva'
 
 
-# preeenchendo os dados
-msg = MIMEMultipart('related')
-msg['From'] = mail_from
-msg['To'] = mail_to
-msg['Subject'] = sub
-msg.attach(MIMEText(body, 'plain'))
-
-
-# envia email
-# importa biblioteca
-import smtplib
-# altorizacao e autenticacao
-smtp = smtplib.SMTP('smtp.gmail.com',587)
-smtp.ehlo()
-smtp.starttls()
-smtp.login(mail_from, pwd)
-smtp.sendmail(mail_from, mail_to, msg.as_string())
-smtp.quit()
